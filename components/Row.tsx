6db2502fb58345ef0d4c4721ea2f03548319915f
SpiderBear <SpiderBear714@gmail.com>
2022-08-01 05:40:29 -0700

    Added gradient styles, sidescroll and row/thumbnail bg-transparency

--- components/Row.tsx
+++ components/Row.tsx
@@ -14,10 +14,15 @@ function Row({ title, movies }: Props) {
   const rowRef = useRef<HTMLDivElement>(null)
   const [isMoved, setIsMoved] = useState(false)
 
-  const handleClick = () => {
+  const handleClick = (direction: string) => {
+    setIsMoved(true)
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current
 
+      const scrollTo = direction === 'left' ? scrollLeft - clientWidth : scrollLeft + clientWidth
+      rowRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' })
+    }
   }
-
 
   return (
     <div className='h-40 space-y-0.5 md:space-y-2'>
@@ -25,7 +30,12 @@ function Row({ title, movies }: Props) {
         {title}
       </h2>
       <div className='group relative md:-ml-2'>
-        <ChevronLeftIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 onClick={() => handleClick{'left'} />
+        <ChevronLeftIcon
+          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
+            !isMoved && 'hidden'
+          }`}
+          onClick={() => handleClick('left')}
+        />
         <div
           ref={rowRef}
           className='flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2 scrollbar-hide'
@@ -34,7 +44,10 @@ function Row({ title, movies }: Props) {
             <Thumbnail key={movie.id} movie={movie} />
           ))}
         </div>
-        <ChevronRightIcon className='absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 onClick={() => handleClick{'right'} />
+        <ChevronRightIcon
+          className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
+          onClick={() => handleClick('right')}
+        />
       </div>
     </div>
   )
